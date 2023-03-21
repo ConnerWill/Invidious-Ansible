20c019f7d407349a8d947890b9ae501d7639fb6e
ConnerWill <connerwill@connerwill.com>
2023-03-20 19:00:56 -0500

    Updated shell script

--- invidious-ansible.sh
+++ invidious-ansible.sh
@@ -1,1 +1,32 @@
 #!/bin/env bash
+
+ACTION="${1}"
+INVENTORY="${2}"
+
+
+## Check action input
+case "${ACTION}" in
+  dep*)
+    playbook="deploy-invidious.yml"
+    ;;
+  upd*)
+    playbook="update-invidious.yml"
+    ;;
+  unin*)
+    playbook="uninstall-invidious.yml"
+    ;;
+  *)
+    echo "INVALID INPUT !!!"
+    ;;
+esac
+
+
+if [[ -z "${INVENTORY}" ]]; then
+  printf "PLEASE SPECIFY AN INVENTORY FILE\n"
+else
+  if [[ ! -f "${INVENTORY}" ]]; then
+    printf "Cannot find inventory file:\t%s\n" "${INVENTORY}"
+  fi
+fi
+
+echo ansible-playbook -i "${INVENTORY}" "./playbooks/${playbook}"
